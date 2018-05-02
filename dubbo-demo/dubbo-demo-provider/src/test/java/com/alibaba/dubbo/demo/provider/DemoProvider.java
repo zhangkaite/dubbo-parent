7ac0b1c0ec4375e3847162449bf9dbbf34534ce7
zhangkaite <zhangkaite@okay.cn>
2018-05-02 18:08:02 +0800

    添加注释及个人理解

--- dubbo-demo/dubbo-demo-provider/src/test/java/com/alibaba/dubbo/demo/provider/DemoProvider.java
+++ dubbo-demo/dubbo-demo-provider/src/test/java/com/alibaba/dubbo/demo/provider/DemoProvider.java
@@ -19,6 +19,8 @@ public class DemoProvider {
 
     public static void main(String[] args) {
         com.alibaba.dubbo.container.Main.main(args);
+        System.out.println("服务启动成功");
     }
+
 
 }
